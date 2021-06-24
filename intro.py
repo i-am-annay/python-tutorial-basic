3b2e3ade70f69be4f91d25ada9398d3b11d53d2e
Annay <paulannay14@gmail.com>
2021-06-24 19:10:59 +0600

    Lesson 2: Working with numeric data

--- intro.py
+++ intro.py
@@ -1,67 +1,58 @@
-# ****String****
-message1 = 'Hello World'        # String in Single Quotes
-message2 = "Hello World"        # String in Double Quotes
+# ****Lesson 2 : Working with numbers in python****
+num = 4
+type(num)   # Returns <class 'int'>
+num = 4.4
+type(num)   # Return <class 'float'>
 
-message3_1 = 'Annay\'s Plan'    # String of Single Quotes containing Single Quotes
-message3_2 = "Annay's Plan"     # String of Double Quotes containing Single Quotes
+# ****Arithmatic Operators****
+# Addition:         3 + 2
+# Subtraction:      3 - 2
+# Multiplication:   3 * 2
+# Division:         3 / 2
+# Floor Division:   3 // 2
+# Exponent:         3 ** 2
+# Modulus:          3 % 2
 
-message4_1 = '"Double" Trouble'  # String of Single Quotes containting Single Quotes
-# String of Double Quotes containing Double Quotes
-message4_2 = "\"Double\" Trouble"
+print(3 + 2)        # Prints 5
+print(3 - 2)        # Prints 1
+print(3 * 2)        # Prints 6
+print(3 / 2)        # Prints 1.5
+print(3 // 2)       # Prints 1
+print(3 ** 2)       # Prints 9
+print(3 % 2)        # Prints 1
 
-# Notes: Backslash (\) is used as an escape Sequence
+# ****Incrementing Value****
+num = 5
+num = num + 1   # Increments by 1 and makes it 6
+num += 1        # Increments by 1 and makes it 7 (Shorthand)
+# num++         # Python does not support ++ or --
+num += 10       # Increments by 10 and makes it 17
+num *= 2        # Multiplies num with 2 and stores it in num
+num /= 2        # Divides num by 2 and stores in in num and converts to float
 
-# ****Multiline String****
-paragraph = """
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-    when an unknown printer took a galley of type and scrambled it to make a type
-    specimen book. It has survived not only five centuries, but also the leap into 
-    electronic typesetting, remaining essentially unchanged. It was popularised in the 
-    1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-    and more recently with desktop publishing software like Aldus PageMaker including versions 
-    of Lorem Ipsum.
-"""
+# ****Built-In functions to work with numbers****
+abs(-12)                    # Absolute Value
+abs(12)                     # Absolute Value
+round(4.75)                 # Rounds to the nearest decimal => 5
+round(4.25)                 # Rounds to the nearest decimal => 4
+round(4.75, 1)              # Rounds to the nearest decimal with 1 sf => 4.8
+round(4.255, 2)             # Rounds to the nearest decimal with 2 sf => 4.25
 
-# ****Slicing Strings****
-name = "Annay Paul"
-name[0]     # Returns the character on the 0th index
-name[1]     # Returns the character on the 1st index
-name[-1]    # Returns the character on the last index
-name[:4]    # Returns the set of characters from the beginning up to the 4th index but not including the 4th index
-name[2:]    # Returns the set of characters from the 2nd index till the end
-name[2:4]   # Returns the set of characters from the 2nd index till the 4th index but not including the 4th index
 
-# ****Getting Length of Strings****
-len(name)   # len() -> built in function that returns the number of characters in the string
+# ****Comparison Operators****
+# Equals:                   3 == 2
+# Not Equals:               3 != 2
+# Greater than:             3 > 2
+# Less than:                3 < 2
+# Greater Than or Equals:   3 >= 2
+# Less Than or Equals:      3 <= 2
 
-# ****Converting into lower case****
-name_lower = name.lower()    # Returns a new string with all characters in lower case
+# ****Converting String to Integer****
+num_1 = '100'
+num_2 = '200'
 
-# ****Converting into upper case****
-name_upper = name.upper()    # Returns a new string with all characters in upper case
+num_1 = int(num_1)
+num_2 = int(num_2)
 
-# ****Counting how many times a character/substring appears****
-name.count('n')  # Returns the number of 'n' in the name 'Annay Paul'
-name.count('au')  # Returns the number of 'au' in the name 'Annay Paul'
-
-# ****Searching the index of a character/substring ****
-name.find('n')   # Returns the index of the 1st occurence of the letter 'n'
-name.find('au')  # Returns the index of the 1st occurence of the substring 'au'
-name.find('x')   # Returns -1 if not found
-
-# ****Replacing a section of a string****
-message1_new = message1.replace('World', 'Universe')
-
-# ****String concatation****
-greeting = 'Hello'
-message = greeting + ', ' + name    # Returns 'Hello, Annay Paul'
-
-# ****Fomratted Strings****
-message = '{}, {}'.format(greeting, name)   # {} act as place holders
-message = f'{greeting}, {name}'  # f-strings, effective for python 3.6>
-
-# ****Finding out documentation****
-print(dir(str))            # Lists all attributes and methods
-print(help(str))           # Lists all attributes and methods with documentation
-print(help(str.lower))     # Provides documentation of the specific attribute
+sum = num_1 + num_2
+print(sum)
